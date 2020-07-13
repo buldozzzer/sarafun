a56313a87a53514e865f1a2f9f5115632c1003f8
2017-3-19-per <van4ester@1.ru>
2020-07-13 23:00:44 +0300

    Initial commit

---
+++ src/main/java/icci/sarafun/SarafunApplication.java
@@ -0,0 +1,13 @@
+package icci.sarafun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarafunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SarafunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/icci/sarafun/SarafunApplicationTests.java
@@ -0,0 +1,13 @@
+package icci.sarafun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarafunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
